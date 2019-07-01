a7313d87c52db6f848c13fcd128f28fc13af3c21
wuttichai pomsua <wuttichai.pomsua@gmail.com>
2019-07-01 11:17:13 +0700

    initial commit

---
+++ CheckEncodingFileApp/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace CheckEncodingFileApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(548, 16);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Browse ...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(507, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(638, 58);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Browse folder";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.Black;
+            this.richTextBox1.Location = new System.Drawing.Point(13, 77);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(1009, 361);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1034, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Check Encoding File Program";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ CheckEncodingFileApp/Form1.cs
@@ -0,0 +1,86 @@
+﻿using CheckEncodingFileApp.SimpleHelpers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CheckEncodingFileApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrEmpty(fbd.SelectedPath))
+                {
+                    bool isNotUTF8 = false;
+
+                    // Clear path textbox
+                    textBox1.Text = "";
+                    textBox1.Text = fbd.SelectedPath;
+
+                    // Clear result textbox
+                    richTextBox1.Clear();
+
+                    string[] files = Directory.GetFiles(fbd.SelectedPath,"*.sql",SearchOption.AllDirectories);
+
+                    if (files.Length>0)
+                    {
+                        Encoding utf8 = Encoding.UTF8;
+                        Encoding t;
+                        foreach (var item in files)
+                        {
+                            string endcode = "";
+                            try
+                            {
+                                t = FileEncoding.DetectFileEncoding(item, utf8);
+                            }
+                            catch (Exception ex)
+                            {
+                                MessageBox.Show("Error " + ex.StackTrace);
+                                return;
+                            }
+
+                            if (!t.EncodingName.Contains("UTF-8"))
+                            {
+                                isNotUTF8 = true;
+                            }
+
+                            endcode = t.EncodingName;
+
+                            richTextBox1.SelectionColor = (t.EncodingName.Contains("UTF-8")?Color.White:Color.Red);
+                            richTextBox1.AppendText(item + "\t" + endcode + System.Environment.NewLine);
+                        }
+
+                        if (isNotUTF8)
+                        {
+                            MessageBox.Show("Some files are not UTF-8 Encoding");
+                        }
+                        else
+                        {
+                            MessageBox.Show("All files are UTF-8 Encoding");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Not found sql file");
+                    }
+
+                }
+            }
+
+        }
+    }
+}
